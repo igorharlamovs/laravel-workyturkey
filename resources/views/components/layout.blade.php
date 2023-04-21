2bea5528b373dfd7d8219583dc56e122b528258c
Igors <iharlamovs@mtx.ie>
2023-04-21 10:33:58 +0100

    [Added] -Installed jQuery

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,8 +3,7 @@
 <link href="style.css" rel="stylesheet">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="path/to/bootstrap-datepicker.css">
-<script src="path/to/bootstrap-datepicker.js"></script>
+<script src="{{ asset('node_modules/jquery/dist/jquery.min.js') }}"></script>
 <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
