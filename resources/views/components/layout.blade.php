2a4a5eda8fa23c567d2ae9d5d9225a6f2d323fdc
Igors <iharlamovs@mtx.ie>
2023-04-21 10:36:40 +0100

    [Added] -Bootstrap

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,9 +3,15 @@
 <link href="style.css" rel="stylesheet">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
-<script src="{{ asset('node_modules/jquery/dist/jquery.min.js') }}"></script>
 <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+
+{{-- jQuery --}}
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="{{ asset('node_modules/jquery/dist/jquery.min.js') }}"></script>
+
+{{-- Bootstrap --}}
+<link rel="stylesheet" href="{{ asset('node_modules/bootstrap/dist/css/bootstrap.min.css') }}">
+<script src="{{ asset('node_modules/bootstrap/dist/js/bootstrap.min.js') }}"></script>
 
 <div class="bg-gray-100 min-h-screen">
     <body style="font-family: Open Sans, sans-serif">
